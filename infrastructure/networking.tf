6e59d639afaf334501a8bd00568c8657e32b1061
Krzysztof Tałajko <krzysztof.talajko@gmail.com>
2017-12-16 14:51:58 +0100

    Terraform subnets

--- infrastructure/networking.tf
+++ infrastructure/networking.tf
@@ -5,3 +5,60 @@ resource "aws_vpc" "hackathon" {
     Name = "hackathon"
   }
 }
+
+resource "aws_subnet" "hackathon_public_a" {
+  vpc_id     = "${aws_vpc.hackathon.id}"
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags {
+    Name = "hackathon"
+  }
+}
+
+resource "aws_subnet" "hackathon_public_b" {
+  vpc_id     = "${aws_vpc.hackathon.id}"
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+
+  tags {
+    Name = "hackathon"
+  }
+}
+
+resource "aws_subnet" "hackathon_public_c" {
+  vpc_id     = "${aws_vpc.hackathon.id}"
+  cidr_block = "10.0.3.0/24"
+  map_public_ip_on_launch = true
+
+  tags {
+    Name = "hackathon"
+  }
+}
+
+resource "aws_subnet" "hackathon_private_a" {
+  vpc_id     = "${aws_vpc.hackathon.id}"
+  cidr_block = "10.0.4.0/24"
+
+  tags {
+    Name = "hackathon"
+  }
+}
+
+resource "aws_subnet" "hackathon_private_b" {
+  vpc_id     = "${aws_vpc.hackathon.id}"
+  cidr_block = "10.0.5.0/24"
+
+  tags {
+    Name = "hackathon"
+  }
+}
+
+resource "aws_subnet" "hackathon_private_c" {
+  vpc_id     = "${aws_vpc.hackathon.id}"
+  cidr_block = "10.0.6.0/24"
+
+  tags {
+    Name = "hackathon"
+  }
+}
